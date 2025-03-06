af758a142c4a91652e3cae2b5d3111631beee0fe
dongwookang <dnehdrkd123@naver.com>
2025-03-06 19:39:30 +0900

    [test]이미지 테스트

--- src/app/banpick/(banpick)/page.tsx
+++ src/app/banpick/(banpick)/page.tsx
@@ -1,4 +1,4 @@
-import ImageComp from "@/components/Image/Image"
+import ImageComp from '@/components/Image/Image';
 interface PropType {
   row?: number;
   col?: number;
@@ -6,11 +6,13 @@ interface PropType {
 
 function Banpick({ row = 5, col = 5 }: PropType) {
   // 0부터 999까지 숫자를 포함하는 배열 생성
-  const arr: number[] = Array.from({ length: 1000 })
+  const arr: number[] = Array.from({ length: 1000 });
 
   return (
     <div className="w-xl h-xl bg-red-50">
-      <ImageComp src="/images/test.png"/>
+      {arr.map((_) => (
+        <ImageComp src="/images/test.png" />
+      ))}
     </div>
   );
 }
