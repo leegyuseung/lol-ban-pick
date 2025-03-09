917dfa053c7abe806fd15e638981a9549c0b20b8
bluke127 <dnehdrkd123@naver.com>
2025-03-10 00:13:53 +0900

    [chore]챔피언 이미지 추가, 외부 이미지 url허용

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['www.banpick.kr'], // 외부 이미지 도메인 추가
+    domains: ['www.banpick.kr',], // 외부 이미지 도메인 추가
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
   },
 };
 
