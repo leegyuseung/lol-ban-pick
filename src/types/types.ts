e5a9a1a680019265c47f3b00cd299728b4f5e27c
leegyuseung <gyus0421@gmail.com>
2025-03-10 12:58:25 +0900

    [feat]types
    FormsData type 추가

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,14 @@
-// types.ts
-
 export interface ApiResponse<T> {
   result_code: number;
   result: T;
   message: string;
 }
+
+export type FormsData = {
+  blueTeam: string;
+  redTeam: string;
+  banpickMode: 'tournament' | 'peerless';
+  peopleMode: 'solo' | 'one' | 'five';
+  timeUnlimited: boolean;
+  teamSide: 'red' | 'blue' | 'solo';
+};
