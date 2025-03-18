eed1761888a1a01475e037618a0c15d7c9328f99
bluke127 <dnehdrkd123@naver.com>
2025-03-18 23:26:35 +0900

    [feat]타입추가

--- src/types/types.ts
+++ src/types/types.ts
@@ -28,3 +28,20 @@ export interface ChampionInfoI {
   status: string;
   line: string[];
 }
+export interface RulesState {
+  myTeam: string;
+  yourTeam: string;
+  banpickMode: 'tournament' | 'peerless3' | 'peerless5';
+  peopleMode: 'solo' | 'team';
+  timeUnlimited: 'true' | 'false';
+  myTeamSide: 'red' | 'blue'; // teamSide가 나의 team을 담고 있어야한다
+  myImg: string;
+  yourImg: string;
+
+  // 피어리스 세트를 담아야한다
+  nowSet: number;
+
+  setRules: (data: FormsData) => void;
+  setPeerlessSet: () => void;
+  setClearPeerlessSet: () => void;
+};
