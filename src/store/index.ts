5f6cbb341dbd30265e2b028ce3bb7ef38ecc71c3
bluke127 <dnehdrkd123@naver.com>
2025-03-18 23:27:11 +0900

    [feat]rule이 변화될때 socket store도 변화 될 수 있도록

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,4 +3,10 @@ import { useRulesStore } from './rules';
 import { useSocketStore } from './socket';
 import { useUserStore } from './user';
 
+useRulesStore.subscribe((state) => {
+  const { myTeam, yourTeam, banpickMode, peopleMode, timeUnlimited, myTeamSide, myImg, yourImg, nowSet } = state;
+  useSocketStore
+    .getState()
+    .setRules({ myTeam, yourTeam, banpickMode, peopleMode, timeUnlimited, myTeamSide, myImg, yourImg, nowSet });
+});
 export { useBanpickStore, useRulesStore, useBanStore, useSocketStore, useUserStore, usePeerlessStore };
