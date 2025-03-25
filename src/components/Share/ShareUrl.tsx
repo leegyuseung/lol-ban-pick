2df7551c956907c908dcb77ca04a97c0ac005def
dongwookang <dnehdrkd123@naver.com>
2025-03-25 12:43:47 +0900

    [style]공유팝업
    cursor pointer

--- src/components/Share/ShareUrl.tsx
+++ src/components/Share/ShareUrl.tsx
@@ -2,12 +2,12 @@ import React, { SyntheticEvent } from 'react';
 import { FaCopy } from 'react-icons/fa6';
 function ShareUrl({ url, role }: { url: string; role: string }) {
   const copyText = (e: SyntheticEvent) => {
-    navigator.clipboard.writeText((e.target as HTMLElement).innerText);
+    navigator.clipboard.writeText(url);
   };
   return (
     <div className='pb-3'>
       <div className='w-full text-left pb-2'>{role} 공유하기</div>
-      <div className='flex items-center space-x-2 border p-2 rounded-lg' onClick={copyText}>
+      <div className='flex items-center space-x-2 border p-2 rounded-lg cursor-pointer' onClick={copyText}>
         <i className='w-5 h-5'>
           <FaCopy />
         </i>
