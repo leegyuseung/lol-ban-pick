7a6379b5b247cf5bb7e887475a7abf62610af355
dongwookang <dnehdrkd123@naver.com>
2025-03-31 10:44:37 +0900

    [build]suspense추가

--- src/app/socketRoom/page.tsx
+++ src/app/socketRoom/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import BanpickSocket from './banpickSocket';
 
 // socketRoom 페이지
@@ -5,7 +6,11 @@ function BanpickSocketWrapper() {
   // 전달된 userId가 없으면 랜덤 값 생성
   const generatedUserId = `${Math.floor(Math.random() * 100000000)}`;
 
-  return <BanpickSocket userId={generatedUserId} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <BanpickSocket userId={generatedUserId} />
+    </Suspense>
+  );
 }
 
 export default BanpickSocketWrapper;
