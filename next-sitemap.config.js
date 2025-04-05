f4ada10dbe623ed7ca9f9cee58901dfb3833eb1d
leegyuseung <lgs4002@naver.com>
2025-04-05 20:14:34 +0900

    [feat] SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://metapick.vercel.app',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
+
+module.exports = config;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
 import Form from '@/components/FormRow';
 import Header from '@/components/Header';
+import { Metadata } from 'next';
 import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: 'MetaPick - 롤 밴픽 시뮬레이터',
+  description: '리그오브레전드의 밴픽을 자유롭게 시뮬레이션할 수 있는 MetaPick!',
+  openGraph: {
+    title: 'MetaPick - 롤 밴픽 시뮬레이터',
+    description: '리그오브레전드의 밴픽을 자유롭게 시뮬레이션할 수 있는 MetaPick!',
+    images: ['/favicon.png'],
+    url: 'https://metapick.vercel.app',
+  },
+};
 
 export default function Home() {
   return (
