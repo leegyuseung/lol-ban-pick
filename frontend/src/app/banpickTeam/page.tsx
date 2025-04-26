3600170c071967b67a49041069cb77cb5f350013
leegyuseung <lgs4002@naver.com>
2025-04-26 15:14:38 +0900

    [fix] SEO 수정

--- frontend/src/app/banpickTeam/page.tsx
+++ frontend/src/app/banpickTeam/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'MetaPick - 팀 모드',
     description: '팀모드 밴픽을 사용중입니다.',
-    images: ['/favicon.png'],
+    images: ['/metapick.png'],
     url: process.env.NEXT_PUBLIC_URL,
   },
 };
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,24 +1,23 @@
 import Form from '@/components/FormRow';
-import Header from '@/components/Header';
 import { Metadata } from 'next';
 import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'MetaPick - 롤 밴픽 시뮬레이터',
-  description: '리그오브레전드의 밴픽을 자유롭게 시뮬레이션할 수 있는 MetaPick!',
+  description:
+    '리그오브레전드의 밴픽을 자유롭게 시뮬레이션할 수 있는 MetaPick! 토너먼트 드리프트, 피어리스 3판, 피어리스 5판 등 다양한 밴픽 모드와 솔로, 팀을 설정하는 참여 모드의 기능이 구현되어 있습니다.',
   openGraph: {
     title: 'MetaPick - 롤 밴픽 시뮬레이터',
-    description: '리그오브레전드의 밴픽을 자유롭게 시뮬레이션할 수 있는 MetaPick!',
-    images: ['/favicon.png'],
+    description:
+      '리그오브레전드의 밴픽을 자유롭게 시뮬레이션할 수 있는 MetaPick! 토너먼트 드리프트, 피어리스 3판, 피어리스 5판 등 다양한 밴픽 모드와 솔로, 팀을 설정하는 참여 모드의 기능이 구현되어 있습니다.',
+    images: ['/metapick.png'],
     url: process.env.NEXT_PUBLIC_URL,
   },
 };
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen">
-      <Header />
-
+    <div className="flex flex-col">
       <Suspense>
         <Form />
       </Suspense>
