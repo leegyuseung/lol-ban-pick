87fae81a22ae9cc32127b2fec89fb64bf42f420a
leegyuseung <lgs4002@naver.com>
2025-04-30 11:53:43 +0900

    [fix] 개발 CSP 수정

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,18 +1,33 @@
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' https://pagead2.googlesyndication.com https://www.googletagmanager.com 'unsafe-inline';
+  script-src 'self' https://pagead2.googlesyndication.com https://www.googletagmanager.com https://ep2.adtrafficquality.google 'unsafe-inline';
   style-src 'self' 'unsafe-inline';
-  img-src 'self' https://ddragon.leagueoflegends.com https://avatars.githubusercontent.com data:;
+  img-src 'self' https://ddragon.leagueoflegends.com https://avatars.githubusercontent.com https://pagead2.googlesyndication.com data:;
   font-src 'self';
-  connect-src 'self' https://pagead2.googlesyndication.com https://www.google-analytics.com https://www.googletagmanager.com;
-  frame-src https://*.google.com https://*.googlesyndication.com;
+  connect-src 'self' https://pagead2.googlesyndication.com https://www.google-analytics.com https://www.googletagmanager.com https://ep1.adtrafficquality.google wss://ws.metapick.site;
+  frame-src https://*.google.com https://*.googlesyndication.com https://googleads.g.doubleclick.net;
   form-action 'self';
   object-src 'none';
-`;
+`
+  .replace(/\s{2,}/g, ' ')
+  .trim();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/(.*)',
+  //       headers: [
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value: ContentSecurityPolicy,
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
   images: {
     remotePatterns: [
       {
@@ -36,19 +51,6 @@ const nextConfig = {
         hostname: 'avatars.githubusercontent.com',
       },
     ],
-    // async headers() {
-    //   return [
-    //     {
-    //       source: '/(.*)',
-    //       headers: [
-    //         {
-    //           key: 'Content-Security-Policy',
-    //           value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
-    //         },
-    //       ],
-    //     },
-    //   ];
-    // },
   },
 };
 
