88488157795398e2ffb72b5ebb5e0d5622a4fa99
leegyuseung <lgs4002@naver.com>
2025-04-30 14:29:32 +0900

    [fix] CSP

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,7 @@ const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' https://pagead2.googlesyndication.com https://www.googletagmanager.com https://ep2.adtrafficquality.google 'unsafe-inline';
   style-src 'self' 'unsafe-inline';
-  img-src 'self' https://ddragon.leagueoflegends.com https://avatars.githubusercontent.com https://pagead2.googlesyndication.com data:;
+  img-src 'self' https://ddragon.leagueoflegends.com https://avatars.githubusercontent.com https://pagead2.googlesyndication.com https://ep1.adtrafficquality.google data:;
   font-src 'self';
   connect-src 'self' https://pagead2.googlesyndication.com https://www.google-analytics.com https://www.googletagmanager.com https://ep1.adtrafficquality.google wss://ws.metapick.site;
   frame-src https://*.google.com https://*.googlesyndication.com https://googleads.g.doubleclick.net https://ep2.adtrafficquality.google;
