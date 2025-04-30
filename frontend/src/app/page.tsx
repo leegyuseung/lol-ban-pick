4314345ee1077af70be0fd0e866dadc3783e0123
leegyuseung <lgs4002@naver.com>
2025-04-30 21:25:40 +0900

    [fix] SEO 최적화

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,11 +6,11 @@ import { Suspense } from 'react';
 export const metadata: Metadata = {
   title: 'MetaPick(메타픽) - 리그오브레전드(롤) 밴픽 시뮬레이터',
   description:
-    '리그오브레전드의 밴픽을 자유롭게 시뮬레이션할 수 있는 MetaPick! 토너먼트 드리프트, 피어리스 3판, 피어리스 5판 등 다양한 밴픽 모드와 솔로, 팀을 설정하는 참여 모드의 기능이 구현되어 있습니다.',
+    '리그오브레전드의 밴픽을 자유롭게 시뮬레이션할 수 있는 MetaPick! 토너먼트 드리프트, 피어리스 등 다양한 밴픽 모드와 솔로, 팀을 설정하는 참여 모드를 이용하세요',
   openGraph: {
     title: 'MetaPick(메타픽) - 리그오브레전드(롤) 밴픽 시뮬레이터',
     description:
-      '리그오브레전드의 밴픽을 자유롭게 시뮬레이션할 수 있는 MetaPick! 토너먼트 드리프트, 피어리스 3판, 피어리스 5판 등 다양한 밴픽 모드와 솔로, 팀을 설정하는 참여 모드의 기능이 구현되어 있습니다.',
+      '리그오브레전드의 밴픽을 자유롭게 시뮬레이션할 수 있는 MetaPick! 토너먼트 드리프트, 피어리스 등 다양한 밴픽 모드와 솔로, 팀을 설정하는 참여 모드를 이용하세요',
     images: [images.THUMBNAIL],
     url: process.env.NEXT_PUBLIC_URL,
     type: 'website',
