b45f382d571d8414dd6f0415e6462cd4bc5360ed
leegyuseung <lgs4002@naver.com>
2025-04-30 10:57:15 +0900

    [feat] CSP 적용

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,3 +1,15 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' https://pagead2.googlesyndication.com https://www.googletagmanager.com 'unsafe-inline';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' https://ddragon.leagueoflegends.com https://avatars.githubusercontent.com data:;
+  font-src 'self';
+  connect-src 'self' https://pagead2.googlesyndication.com https://www.google-analytics.com https://www.googletagmanager.com;
+  frame-src https://*.google.com https://*.googlesyndication.com;
+  form-action 'self';
+  object-src 'none';
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -24,6 +36,19 @@ const nextConfig = {
         hostname: 'avatars.githubusercontent.com',
       },
     ],
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+            },
+          ],
+        },
+      ];
+    },
   },
 };
 
